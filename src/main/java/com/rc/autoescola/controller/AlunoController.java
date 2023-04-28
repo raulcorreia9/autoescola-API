da70c9b6a941c47968fde43a4817e6b225fecd4e
Raul Correia <raulvinicius245@gmail.com>
2023-04-27 21:10:38 -0300

    feat: Adicionado endpoint para buscar Aluno por ID. Criado também exception pernosalizada.

--- src/main/java/com/rc/autoescola/controller/AlunoController.java
+++ src/main/java/com/rc/autoescola/controller/AlunoController.java
@@ -1,19 +1,24 @@
 package com.rc.autoescola.controller;
 
 import com.rc.autoescola.DTO.AlunoDTO;
+import com.rc.autoescola.exception.ErrorResponseDetails;
+import com.rc.autoescola.exception.NotFoundException;
 import com.rc.autoescola.models.Aluno;
 import com.rc.autoescola.service.AlunoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/autoescola")
+@RequestMapping(value = "/api/aluno")
 public class AlunoController {
     private final AlunoService alunoService;
 
@@ -22,8 +27,29 @@ public class AlunoController {
         return ResponseEntity.ok(alunoService.findAll());
     }
 
+    @GetMapping("/paginated")
+    public ResponseEntity<Page<Aluno>> findAllPaginated(Pageable pageable) {
+        return ResponseEntity.ok(alunoService.findAllPaginated(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Aluno> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(alunoService.findById(id));
+    }
+
+//    @ExceptionHandler(NotFoundException.class)
+//    public ResponseEntity<ErrorResponseDetails> handleAlunoNaotFoundException(NotFoundException ex) {
+//        ErrorResponseDetails error = ErrorResponseDetails.builder()
+//                .status(HttpStatus.NOT_FOUND.value())
+//                .message(ex.getMessage())
+//                .timestamp(LocalDateTime.now())
+//                .build();
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+//    }
+
     @PostMapping
     public ResponseEntity<Aluno> save(@Valid @RequestBody AlunoDTO alunoDTO) {
         return new ResponseEntity<>(alunoService.save(alunoDTO), HttpStatus.CREATED);
     }
+
 }
--- src/main/java/com/rc/autoescola/service/AlunoService.java
+++ src/main/java/com/rc/autoescola/service/AlunoService.java
@@ -1,10 +1,13 @@
 package com.rc.autoescola.service;
 
 import com.rc.autoescola.DTO.AlunoDTO;
+import com.rc.autoescola.exception.NotFoundException;
 import com.rc.autoescola.models.Aluno;
 import com.rc.autoescola.repository.AlunoRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -17,14 +20,22 @@ public class AlunoService {
     private final AlunoRepository alunoRepository;
     private final ModelMapper modelMapper;
 
+    public List<Aluno> findAll() {
+        return alunoRepository.findAll();
+    }
+
+    public Page<Aluno> findAllPaginated(Pageable pageable) {
+        return alunoRepository.findAll(pageable);
+    }
+
+    public Aluno findById(Long id) {
+        return alunoRepository.findById(id).orElseThrow(() -> new NotFoundException("Aluno não encontrado."));
+    }
+
     @Transactional
     public Aluno save(AlunoDTO alunoDTO) {
         Aluno aluno = modelMapper.map(alunoDTO, Aluno.class);
         return alunoRepository.save(aluno);
-    }
-
-    public List<Aluno> findAll() {
-        return alunoRepository.findAll();
     }
 
 }
