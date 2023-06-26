694ef8417eb97a45a8b6fd85467fda4c11acc8b8
Raul Correia <raulvinicius245@gmail.com>
2023-06-26 19:42:41 -0300

    feat: Criado entidade Instrutor e seu relacionamento com veiculo.

---
+++ src/main/java/com/rc/autoescola/domain/models/Instrutor.java
@@ -0,0 +1,35 @@
+package com.rc.autoescola.domain.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Instrutor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String cpf;
+
+    @ManyToMany
+    @JsonIgnore
+    @JoinTable(name = "instrutor_veiculo",
+            joinColumns = @JoinColumn(name = "instrutor_id"),
+            inverseJoinColumns = @JoinColumn(name = "veiculo_id"))
+    private List<Veiculo> veiculos = new ArrayList<>();
+}
